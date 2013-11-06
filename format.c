bb0028f864b03cc1a8f71ffd95b640325e51e6d6
Alvaro Reis Neto <reisneto.alvaro@gmail.com>
2013-11-05 23:35:08 -0600

    justifying working

--- format.c
+++ format.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-FILE *open_file(char *filename);
+
+#define MAX 101
+FILE *open_file(char *filename, char type[]);
 void print_doc(FILE *doc);
 	
 int main(int argc, char *argv[])
 {
 	int line_size;
 	FILE *doc;
+	FILE *doc_out;
+	char word[MAX];
+	int counter;
+
+
 	if(argc != 3)
 	{
 		fprintf(stderr, "Usage: ./format line_size file\n");
@@ -16,27 +24,57 @@ int main(int argc, char *argv[])
 	
 	line_size = atoi(argv[1]);
 
-	doc = open_file(argv[2]);
-	print_doc(doc);
+	doc = open_file(argv[2],"r");
+
+	doc_out = open_file(strcat(argv[2],".out"),"w"); 
+
+	/** algorithm
+	 handle lines with a variable
+	 **/
+
+	/* Assuming that there isn't a word greather than 25 chars */
+	fscanf(doc,"%s",word);
+	fprintf(doc_out,"%s", word);
+	counter = strlen(word);
+
+	while( (fscanf(doc,"%s",word) == 1) )
+	{
+			if(counter + strlen(word) < line_size)
+			{
+				fprintf(doc_out," %s",word);
+				counter += strlen(word);
+			}
+			else
+			{
+				fprintf(doc_out,"\n%s",word);
+				counter=0;
+			}
+	}
 	fclose(doc);
+	fclose(doc_out);
+
+	doc_out = open_file(argv[2],"r"); 
+
+	print_doc(doc_out); 
+
 	return 0;
 }
 
 void print_doc(FILE *doc)
 {
-	char line[100];
+	char line[MAX];
 	while(!feof(doc))
 	{
-		if( fgets(line, 100, doc) == NULL ) break;
+		if( fgets(line, MAX, doc) == NULL ) break;
 		printf("%s",line);
 	}
 	return;
 }
 
-FILE *open_file(char *filename)
+FILE *open_file(char *filename,char type[])
 {
 	FILE *doc;
-	doc = fopen(filename, "r");
+	doc = fopen(filename, type);
 	if (doc == NULL) 
 	{
 		perror("Error opening file");
