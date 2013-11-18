907070d81a7ab9bfe3a0bbda16fece3f9f302071
Alvaro Reis Neto <reisneto.alvaro@gmail.com>
2013-11-18 14:34:19 -0600

    list of words being processed

--- format.c
+++ format.c
@@ -6,7 +6,9 @@
 #define MAX 101
 FILE *open_file(char *filename, char type[]);
 void print_doc(FILE *doc);
-	
+int compare(const void* a,const void* b);
+int wordCount(char ***wordlist, int *numwords, char word[]);
+
 int main(int argc, char *argv[])
 {
 	int line_size;
@@ -14,7 +16,11 @@ int main(int argc, char *argv[])
 	FILE *doc_out;
 	char word[MAX];
 	int counter;
+	int numwords=1;
+	int i;
+	char **wordlist = malloc(sizeof(char*));
 
+	wordlist[0] = "default";
 
 	if(argc != 3)
 	{
@@ -37,6 +43,7 @@ int main(int argc, char *argv[])
 	fprintf(doc_out,"%s", word);
 	counter = strlen(word);
 
+	/*** JUSTIFY TEXT ***/
 	while( (fscanf(doc,"%s",word) == 1) )
 	{
 			if(counter + strlen(word) < line_size)
@@ -49,13 +56,24 @@ int main(int argc, char *argv[])
 				fprintf(doc_out,"\n%s",word);
 				counter=0;
 			}
+			wordCount(&wordlist, &numwords, word);
 	}
+
 	fclose(doc);
 	fclose(doc_out);
 
+
+	/*** PRINT OUT JUSTIFIED TEXT ***/
 	doc_out = open_file(argv[2],"r"); 
 
 	print_doc(doc_out); 
+
+	/*qsort((char *) wordlist, numwords, sizeof(wordlist[0]), compare);*/
+
+	printf( "\n\nAdded %d words to the array and they are:\n", numwords );
+	for( i = 0; i < numwords; i++ )
+		printf( "%s\n", wordlist[ i ] );
+
 
 	return 0;
 }
@@ -85,4 +103,25 @@ FILE *open_file(char *filename,char type[])
 	{
 		return doc;
 	}
+}
+
+
+int wordCount(char ***wordlist, int *numwords, char word[])
+/* Put word inside wordlist, even if there's already a copy of it */
+{
+	/* Expand our array of pointers by one pointer's worth */
+	*wordlist = realloc( *wordlist, (*numwords + 1) * sizeof( char * ) );
+	if(*wordlist == NULL)
+		return 0;
+	
+	/* Make a duplicate of the word and save the pointer to it */
+	(*wordlist)[ *numwords ] = strdup( word);
+	(*numwords)++;	
+	return 1;
+}
+
+int compare(const void* a,const void* b)
+{
+	return(strcmp( *( (char**)a),*( (char**)b) ) );
+
 }
