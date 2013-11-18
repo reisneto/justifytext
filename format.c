7b40ff23efe4712a78e945015511cf0f3bcde5c2
Alvaro Reis Neto <reisneto.alvaro@gmail.com>
2013-11-18 15:37:30 -0600

    file filename.words created showing words 'ASCII ordered'

--- format.c
+++ format.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 
 #define MAX 101
@@ -8,6 +9,7 @@ FILE *open_file(char *filename, char type[]);
 void print_doc(FILE *doc);
 int compare(const void* a,const void* b);
 int wordCount(char ***wordlist, int *numwords, char word[]);
+void printWordCounted(char **wordlist, int numwords, char *filename);
 
 int main(int argc, char *argv[])
 {
@@ -17,7 +19,7 @@ int main(int argc, char *argv[])
 	char word[MAX];
 	int counter;
 	int numwords=1;
-	int i;
+	char filename[MAX];
 	char **wordlist = malloc(sizeof(char*));
 
 	wordlist[0] = "default";
@@ -32,7 +34,9 @@ int main(int argc, char *argv[])
 
 	doc = open_file(argv[2],"r");
 
-	doc_out = open_file(strcat(argv[2],".out"),"w"); 
+	strcpy(filename,argv[2]);
+	strcat(filename,".out");
+	doc_out = open_file(filename,"w"); 
 
 	/** algorithm
 	 handle lines with a variable
@@ -62,18 +66,14 @@ int main(int argc, char *argv[])
 	fclose(doc);
 	fclose(doc_out);
 
-
 	/*** PRINT OUT JUSTIFIED TEXT ***/
-	doc_out = open_file(argv[2],"r"); 
+/*	doc_out = open_file(argv[2],"r"); 
 
 	print_doc(doc_out); 
+*/
+	qsort((char *) wordlist, numwords, sizeof(wordlist[0]), compare);
 
-	/*qsort((char *) wordlist, numwords, sizeof(wordlist[0]), compare);*/
-
-	printf( "\n\nAdded %d words to the array and they are:\n", numwords );
-	for( i = 0; i < numwords; i++ )
-		printf( "%s\n", wordlist[ i ] );
-
+	printWordCounted(wordlist, numwords, argv[2]);
 
 	return 0;
 }
@@ -122,6 +122,35 @@ int wordCount(char ***wordlist, int *numwords, char word[])
 
 int compare(const void* a,const void* b)
 {
-	return(strcmp( *( (char**)a),*( (char**)b) ) );
+	return(strcmp( *(  (char**)a ), *( (char**)b ) ) );
 
+}
+
+void printWordCounted(char **wordlist, int numwords, char *filename)
+{
+	int i, countWord=1;
+	char current[MAX],previous[MAX];
+	FILE* doc_out;
+
+	strcat(filename,".words");
+
+	doc_out = open_file(filename,"w");
+
+	strcpy(previous, wordlist[0]);
+
+	for( i = 1; i < numwords; i++ )
+	{
+		strcpy(current, wordlist[i]);
+		if(strstr(current,previous) == current && ( strlen(current) == strlen(previous) )) /* if they are the same word, count +1 */
+		{
+			countWord++;
+		}else
+		{
+			fprintf(doc_out, "%s - %d\n", previous, countWord );
+			countWord=1;
+		}
+		strcpy(previous,current);
+	}
+	fclose(doc_out);
+	return;
 }
